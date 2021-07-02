20ace53c7e5f57e082a0651ca645a8341c4db47b
kw00183 <kw00183@cs6261-vm05.uwg.westga.edu>
2021-07-02 13:50:37 -0400

    add capabilities for firefox to protractor config file and create new routing.e2e-spec.ts

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -9,11 +9,11 @@ exports.config = {
     './src/**/*.e2e-spec.ts'
   ],
   capabilities: {
-    'browserName': 'firefox',
-    'moz:firefoxOptions': {
-      'args' : [ '--headless' ]
-    }
-  },
+    'browserName' : 'firefox',
+    'moz:firefoxOptions' : {
+    'args' : [ '--headless' ]
+  }
+},
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
